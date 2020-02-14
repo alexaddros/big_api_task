54afbdcee60d41e492701b0374e53f4e663dcb7a
alexaddros <skopcovs@gmail.com>
2020-02-14 17:01:19 +0300

    init commit

--- main.py
+++ main.py
@@ -1,5 +1,52 @@
-import sys
-import math
+import os
+import pygame
 import requests
-from PIL import Image
-from io import BytesIO
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('path', type=str)
+parser.add_argument('--spn', type=str, default='0.02')
+arguments = parser.parse_args().__dict__
+
+toponym_to_find = arguments['path']
+delta = arguments['spn']
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+geocoder_params = {
+    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+    "geocode": toponym_to_find,
+    "format": "json"}
+
+response = requests.get(geocoder_api_server, params=geocoder_params)
+
+if not response:
+    raise Exception
+
+json_response = response.json()
+toponym = json_response["response"]["GeoObjectCollection"][
+    "featureMember"][0]["GeoObject"]
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
+}
+
+
+
+response = requests.get(map_api_server, params=map_params)
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
